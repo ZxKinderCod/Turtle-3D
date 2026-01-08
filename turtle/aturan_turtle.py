217dd06a76e7364eda8bde4a3cdcc079d88e08e0
Gigih Ridho Apriansyah <gigihridhoa20@gmail.com>
2026-01-08 21:00:35 +0700

    update

---
+++ turtle/aturan_turtle.py
@@ -0,0 +1,5 @@
+#goto(x, y) = gambar garis ke koordinat
+#penup() + goto() + pendown() = pindah tanpa gambar
+#forward(jarak) = maju sesuai arah
+#setheading(sudut) = atur arah (0°=kanan, 90°=atas, dll)
+#for loop = ulang perintah
